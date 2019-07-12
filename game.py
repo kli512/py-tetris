eeef0b6c60d899970b8bcda3ffa87dd426c52590
kli512 <li.kevin512@gmail.com>
2019-07-12 17:45:12 -0500

    Moved game logic into game handler file

--- game.py
+++ game.py
@@ -1,101 +1,12 @@
-import sys
 import pygame
 from tetris import Board
-from pygame_handler import GraphicsHandler
+from pygame_handler import GameHandler
 
 def main():
     pygame.init()
-    gh = GraphicsHandler()
-    m_board = Board()
+    gh = GameHandler(Board())
 
-    gh.draw_main_menu()
-
-    menuing = True
-
-    while menuing:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.KEYDOWN:
-                menuing = False
-
-    running = True
-
-    clock = pygame.time.Clock()
-    until_falling = 1000
-
-    gravity = 1
-
-    heldkeys = set()
-
-    c_time = pygame.time.get_ticks()
-
-    while running:
-        o_time = c_time
-        clock.tick()
-
-        c_time = pygame.time.get_ticks()
-        delta_t = c_time - o_time
-
-        # auto falling code
-        until_falling -= gravity * delta_t
-        if until_falling < 0:
-            until_falling = 1000
-            m_board.act('d')
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
-                sys.exit()
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    m_board.act('l')
-                elif event.key == pygame.K_RIGHT:
-                    m_board.act('r')
-
-                if event.key == pygame.K_UP:
-                    m_board.act('cw')
-                elif event.key == pygame.K_z:
-                    m_board.act('ccw')
-
-                if event.key == pygame.K_DOWN:
-                    m_board.act('d')
-                    until_falling = 1000
-                elif event.key == pygame.K_SPACE:
-                    m_board.act('hd')
-                    until_falling = 1000
-
-                if event.key == pygame.K_LSHIFT:
-                    m_board.act('hold')
-        
-        gh.draw_game(m_board)
-        pygame.display.update()
-
-        if m_board.dead:
-            running = False
-    
-    gh.draw_lose(m_board.score)
-    pygame.display.update()
-
-    waiting = True
-
-    while waiting:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                waiting = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    waiting = False
-    
-    pygame.display.quit()
-    pygame.quit()
-    sys.exit()
+    gh.play_game()
 
 if __name__ == "__main__":
     main()
--- pygame_handler.py
+++ pygame_handler.py
@@ -1,11 +1,24 @@
+import sys
 import pygame
 from tetris import Board
 import utils
 
 
-class GraphicsHandler:
-    def __init__(self):
-        pygame.display.set_caption('teetris')
+class GameHandler:
+    def __init__(self, board):
+        pygame.display.set_caption('Teetris')
+
+        self.m_board = board
+
+        self.key_map = {
+            pygame.K_LEFT: 'l',
+            pygame.K_RIGHT: 'r',
+            pygame.K_UP: 'cw',
+            pygame.K_z: 'ccw',
+            pygame.K_DOWN: 'd',
+            pygame.K_SPACE: 'hd',
+            pygame.K_LSHIFT: 'hold'
+        }
 
         self.win_width = 800
         self.win_height = 700
@@ -14,6 +27,12 @@ class GraphicsHandler:
         self.block_size = 30
 
         self.win = pygame.display.set_mode((self.win_width, self.win_height))
+
+    def _top_left_x(self):
+        return (self.win_width - self.play_width) / 2
+
+    def _top_left_y(self):
+        return (self.win_height - self.play_height) / 2
 
     def _draw_text(self, text, pos, size=40, color=(255, 255, 255), bold=False):
         font = pygame.font.SysFont('arial', size, bold=bold)
@@ -30,13 +49,15 @@ class GraphicsHandler:
         self.win.blit(label, textpos)
 
     def _draw_text_middle(self, text, size=40, color=(255, 255, 255), bold=False):
-        pos = (self.top_left_x() + self.play_width / 2,
-               self.top_left_y() + self.play_height / 2)
+        pos = (self._top_left_x() + self.play_width / 2,
+               self._top_left_y() + self.play_height / 2)
 
         self._draw_centered_text(text, pos, size, color, bold)
 
-    def _draw_board(self, bstate):
-        tlx, tly = self.top_left_x(), self.top_left_y()
+    def _draw_board(self):
+        tlx, tly = self._top_left_x(), self._top_left_y()
+
+        bstate = self.m_board.state()
 
         for i in range(len(bstate)):
             for j in range(len(bstate[0])):
@@ -56,25 +77,26 @@ class GraphicsHandler:
                     pygame.draw.rect(self.win, color, (tlx + j * self.block_size,
                                                        tly + i * self.block_size, self.block_size, self.block_size), 0)
 
-    def _draw_next_box(self, piece_str):
-        tlx, tly = self.top_left_x() + self.play_width + 50, self.top_left_y() + 50
-        self._draw_piece_box(piece_str, tlx, tly)
+    def _draw_next_box(self):
+        tlx, tly = self._top_left_x() + self.play_width + 50, self._top_left_y() + 50
+        self._draw_piece_box(self.m_board.nextPiece, tlx, tly)
 
-    def _draw_hold_box(self, piece_str):
-        if piece_str == None:
+    def _draw_hold_box(self):
+        held = self.m_board.held_piece
+        if held is None:
             return
-        tlx, tly = self.top_left_x() - 150, self.top_left_y() + 50
-        self._draw_piece_box(piece_str, tlx, tly)
+        tlx, tly = self._top_left_x() - 150, self._top_left_y() + 50
+        self._draw_piece_box(held, tlx, tly)
 
-    def _draw_score(self, score):
+    def _draw_score(self):
         self._draw_text('Score: {}'.format(
-            score), (self.top_left_x(), self.top_left_y() + self.play_height))
+            self.m_board.score), (self._top_left_x(), self._top_left_y() + self.play_height))
 
     def _clear_board(self):
         self.win.fill((0, 0, 0))
 
     def _draw_grid(self):
-        tlx, tly = self.top_left_x(), self.top_left_y()
+        tlx, tly = self._top_left_x(), self._top_left_y()
         for i in range(20):
             pygame.draw.line(self.win, (50, 50, 50), (tlx, tly + i*30),
                              (tlx + self.play_width, tly + i * 30))  # horizontal lines
@@ -82,57 +104,98 @@ class GraphicsHandler:
                 pygame.draw.line(self.win, (50, 50, 50), (tlx + j * 30, tly),
                                  (tlx + j * 30, tly + self.play_height))  # vertical lines
 
-    def draw_game(self, board):
+    def _draw_game(self):
         self._clear_board()
-        self._draw_board(board.state())
+        self._draw_board()
         self._draw_grid()
-        self._draw_score(board.score)
-        self._draw_next_box(board.nextPiece)
-        self._draw_hold_box(board.held_piece)
+        self._draw_score()
+        self._draw_next_box()
+        self._draw_hold_box()
         # implement more drawings for the game e.g. controls
 
-    def draw_main_menu(self):
+    def _draw_main_menu(self):
         self._clear_board()
         self._draw_text_middle('Press any key', bold=True)
-        pygame.display.update()\
+        pygame.display.update()
 
-
-    def draw_lose(self, score):
+    def _draw_lose(self):
         self._clear_board()
         self._draw_text_middle(
-            'You Lost\nYour Score was {}'.format(score), bold=True)
+            'You Lost\nYour Score was {}'.format(self.m_board.score), bold=True)
 
-    def top_left_x(self):
-        return (self.win_width - self.play_width) / 2
+    def _menu_loop(self):
+        self._draw_main_menu()
+        menuing = True
 
-    def top_left_y(self):
-        return (self.win_height - self.play_height) / 2
+        while menuing:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.display.quit()
+                    pygame.quit()
+                    sys.exit()
 
-# tester code follows
+                if event.type == pygame.KEYDOWN:
+                    menuing = False
 
+    def _game_loop(self):
+        running = True
 
-def main():
-    pygame.init()
-    gh = GraphicsHandler()
+        clock = pygame.time.Clock()
+        until_falling = 1000
 
-    b = Board()
+        gravity = 1
 
-    gh.draw_main_menu()
-    run = True
-    while run:
+        c_time = pygame.time.get_ticks()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+        while running:
+            o_time = c_time
+            clock.tick()
 
-            if event.type == pygame.KEYDOWN:
-                gh.draw_game(b)
-                pygame.display.update()
+            c_time = pygame.time.get_ticks()
+            delta_t = c_time - o_time
 
-    pygame.display.quit()
-    pygame.quit()
-    quit()
+            # auto falling code
+            until_falling -= gravity * delta_t
+            if until_falling < 0:
+                until_falling = 1000
+                self.m_board.act('d')
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.display.quit()
+                    pygame.quit()
+                    sys.exit()
 
-if __name__ == "__main__":
-    main()
+                if event.type == pygame.KEYDOWN:
+                    self.m_board.act(self.key_map[event.key])
+
+            self._draw_game()
+            pygame.display.update()
+
+            if self.m_board.dead:
+                running = False
+    
+    def _end_loop(self):
+        self._draw_lose()
+        pygame.display.update()
+
+        waiting = True
+
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    waiting = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        waiting = False
+        
+        pygame.display.quit()
+        pygame.quit()
+        sys.exit()
+
+    def play_game(self):
+        pygame.init()
+        self._menu_loop()
+        self._game_loop()
+        self._end_loop()
