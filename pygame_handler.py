849a061c81b13455e1d529754beea59c7b464225
kli512 <li.kevin512@gmail.com>
2019-07-19 13:03:47 -0500

    Added ghost piece, 5 preview pieces, soft drop arr, finished SRS, 7-bag randomizer

--- pygame_handler.py
+++ pygame_handler.py
@@ -34,52 +34,60 @@ class GameHandler:
     def _top_left_y(self):
         return (self.win_height - self.play_height) / 2
 
-    def _draw_text(self, text, pos, size=40, color=(255, 255, 255), bold=False):
+    def _draw_text(self, text, pos, align=(-1, -1), size=40, color=(255, 255, 255), bold=False):
         font = pygame.font.SysFont('arial', size, bold=bold)
         label = font.render(text, 1, color)
 
-        self.win.blit(label, pos)
-
-    def _draw_centered_text(self, text, pos, size=40, color=(255, 255, 255), bold=False):
-        font = pygame.font.SysFont('arial', size, bold=bold)
-        label = font.render(text, 1, color)
-        textpos = (pos[0] - label.get_width() / 2,
-                   pos[1] - label.get_height() / 2)
+        textpos = list(pos)
+        if align[0] == 0:
+            textpos[0] -= label.get_width() / 2
+        elif align[0] == 1:
+            textpos[0] -= label.get_width()
+        
+        if align[1] == 0:
+            textpos[1] -= label.get_height() / 2
+        elif align[1] == 1:
+            textpos[1] -= label.get_height()
 
         self.win.blit(label, textpos)
-
-    def _draw_text_middle(self, text, size=40, color=(255, 255, 255), bold=False):
-        pos = (self._top_left_x() + self.play_width / 2,
-               self._top_left_y() + self.play_height / 2)
-
-        self._draw_centered_text(text, pos, size, color, bold)
 
     def _draw_board(self):
         tlx, tly = self._top_left_x(), self._top_left_y()
 
-        bstate = self.m_board.state()
+        bstate = self.m_board.state()[(
+            self.m_board.height - self.m_board.playable_height)::]
 
         for i in range(len(bstate)):
             for j in range(len(bstate[0])):
                 pygame.draw.rect(self.win, utils.VAL_TO_COLOR[bstate[i][j]], (
-                    tlx + j * self.block_size, tly + i * self.block_size, self.block_size, self.block_size), 0)
+                    tlx + j * self.block_size, tly + i * self.block_size, self.block_size, self.block_size))
+
+        for pos in self.m_board.ghost_piece_occupied:
+            if pos[0] >= self.m_board.height - self.m_board.playable_height:
+                s = pygame.Surface((self.block_size, self.block_size))
+                s.set_alpha(128)
+                s.fill(
+                    utils.VAL_TO_COLOR[utils.shape_values[self.m_board.cur_piece.piece_str]])
+                self.win.blit(s, (tlx + pos[1] * self.block_size,
+                            tly + (pos[0] - 6) * self.block_size))
 
         pygame.draw.rect(self.win, (169, 169, 169), (tlx, tly,
                                                      self.play_width, self.play_height), 5)
 
     def _draw_piece_box(self, piece_str, tlx, tly):
-        shape = utils.shapes[piece_str]
+        shape = utils.LETTER_TO_SHAPE[piece_str]
         color = utils.VAL_TO_COLOR[utils.shape_values[piece_str]]
 
         for i in range(len(shape)):
             for j in range(len(shape)):
                 if shape[i][j] != 0:
                     pygame.draw.rect(self.win, color, (tlx + j * self.block_size,
-                                                       tly + i * self.block_size, self.block_size, self.block_size), 0)
+                                                       tly + i * self.block_size, self.block_size, self.block_size))
 
-    def _draw_next_box(self):
+    def _draw_next_boxs(self):
         tlx, tly = self._top_left_x() + self.play_width + 50, self._top_left_y() + 50
-        self._draw_piece_box(self.m_board.next_piece, tlx, tly)
+        for i in range(5):
+            self._draw_piece_box(self.m_board.next_pieces[i], tlx, tly + i * self.block_size * 3)
 
     def _draw_hold_box(self):
         held = self.m_board.held_piece
@@ -87,6 +95,9 @@ class GameHandler:
             return
         tlx, tly = self._top_left_x() - 150, self._top_left_y() + 50
         self._draw_piece_box(held, tlx, tly)
+
+    def _draw_lines_cleared(self):
+        self._draw_text('Lines: {}'.format(self.m_board.lines_cleared), (self._top_left_x() + self.play_width/2, self._top_left_y()), (0, 1))
 
     def _draw_score(self):
         self._draw_text('Score: {}'.format(
@@ -97,10 +108,10 @@ class GameHandler:
 
     def _draw_grid(self):
         tlx, tly = self._top_left_x(), self._top_left_y()
-        for i in range(20):
+        for i in range(self.m_board.playable_height):
             pygame.draw.line(self.win, (50, 50, 50), (tlx, tly + i*30),
                              (tlx + self.play_width, tly + i * 30))  # horizontal lines
-            for j in range(10):
+            for j in range(self.m_board.width):
                 pygame.draw.line(self.win, (50, 50, 50), (tlx + j * 30, tly),
                                  (tlx + j * 30, tly + self.play_height))  # vertical lines
 
@@ -109,19 +120,19 @@ class GameHandler:
         self._draw_board()
         self._draw_grid()
         self._draw_score()
-        self._draw_next_box()
+        self._draw_lines_cleared()
+        self._draw_next_boxs()
         self._draw_hold_box()
         # implement more drawings for the game e.g. controls
 
     def _draw_main_menu(self):
         self._clear_board()
-        self._draw_text_middle('Press any key', bold=True)
+        self._draw_text('Press any key', (self.win_width / 2, self.win_height / 2), (0,0), bold=True)
         pygame.display.update()
 
     def _draw_lose(self):
         self._clear_board()
-        self._draw_text_middle(
-            'You Lost\nYour Score was {}'.format(self.m_board.score), bold=True)
+        self._draw_text('Your Score was {}'.format(self.m_board.score), (self.win_width / 2, self.win_height / 2), (0,0), bold=True)
 
     def _get_das_keys(self):
         keys = []
@@ -155,14 +166,15 @@ class GameHandler:
 
         das = 100
         arr = 1
+        soft_drop_rate = 1
 
-        held_time = defaultdict()
-        arr_charge = defaultdict()
+        held_time = defaultdict(lambda: 0)
+        arr_charge = defaultdict(lambda: 0)
 
         das_keys = self._get_das_keys()
 
         c_time = pygame.time.get_ticks()
-        
+
         until_falling = 1000
         lock_delay = 500
 
@@ -176,16 +188,15 @@ class GameHandler:
             # auto falling code
             until_falling -= gravity * delta_t
             if until_falling <= 0:
-                if self.m_board.should_lock():
+                if self.m_board.act('d') == 0:
                     if lock_delay <= 0:
+                        self.m_board.lock_piece()
                         until_falling = 1000
-                        self.m_board.act('d')
 
                     lock_delay -= delta_t
                 else:
                     # lock_delay = 500 # SEGA rotation lock delay
                     until_falling = 1000
-                    self.m_board.act('d')
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -196,14 +207,18 @@ class GameHandler:
                 if event.type == pygame.KEYDOWN:
                     try:
                         if self.m_board.act(self.key_map[event.key]) == 1:
-                            lock_delay = 500 # Infinity lock delay
+                            if self.key_map[event.key] == 'hold':
+                                until_falling = 1000
+                            lock_delay = 500  # Infinity lock delay
                     except KeyError:
-                        continue # skips this key event
+                        continue  # skips this key event
 
                     if event.key in das_keys:
                         for key in das_keys:
                             held_time[key] = -delta_t
                             arr_charge[key] = -delta_t
+                    elif event.key == pygame.K_DOWN:
+                        arr_charge[event.key] = -delta_t
 
             keys = pygame.key.get_pressed()
 
@@ -216,6 +231,13 @@ class GameHandler:
                         while arr_charge[key] > arr:
                             arr_charge[key] -= arr
                             self.m_board.act(self.key_map[key])
+
+            # should use a member variable/function that is the inverse mapping of key_map
+            if keys[pygame.K_DOWN]:
+                arr_charge[pygame.K_DOWN] += delta_t
+                while arr_charge[pygame.K_DOWN] > soft_drop_rate:
+                    arr_charge[pygame.K_DOWN] -= soft_drop_rate
+                    self.m_board.act('d')
 
             self._draw_game()
             pygame.display.update()
--- tetris.py
+++ tetris.py
@@ -1,32 +1,36 @@
 import copy
 import random
+from collections import deque
 from datetime import datetime
 import os
+import sys
 
 import numpy as np
 import utils
 
 class Piece:
-
     # called with the piece_str e.g. 'S' representing which piece
     # can be I, J, L, O , S, T, Z pieces
-    def __init__(self, piece_str):
+    def __init__(self, piece_str, r=5):
         self.piece_str = piece_str
-        self._shape = utils.shapes[piece_str] # the rotation is implicit in _shape
+        self._shape = utils.LETTER_TO_SHAPE[piece_str]
+        self.rotation = 0
 
         # sets the spawning location (compensates for O's small size by moving it forward on column)
-        self._pos = [0, 3]
+        self.pos = [r, 3]
         if piece_str == 'O':
-            self._pos[1] += 1
+            self.pos[1] += 1
 
     # rotates the piece (rotates the _shape matrix)
-    # -1 is ccw, 1 is cw
+    # utils.CLOCKWISE and utils.COUNTERCLOCKWISE define rotation
     def _rotate(self, direc):
-        if direc != -1 and direc != 1:
+        if direc not in (utils.CLOCKWISE, utils.COUNTERCLOCKWISE):
             return False
 
+        self.rotation = (self.rotation + direc) % 4
+
         self._shape = list(map(list, zip(*self._shape[::-1])))
-        if direc == -1:
+        if direc == utils.COUNTERCLOCKWISE:
             self._shape = list(map(list, zip(*self._shape[::-1])))
             self._shape = list(map(list, zip(*self._shape[::-1])))
         return True
@@ -35,13 +39,13 @@ class Piece:
     # 'd', 'l', or 'r'
     def _move(self, direc):
         if direc == 'u':
-            self._pos[0] -= 1
+            self.pos[0] -= 1
         elif direc == 'd':
-            self._pos[0] += 1
+            self.pos[0] += 1
         elif direc == 'l':
-            self._pos[1] -= 1
+            self.pos[1] -= 1
         elif direc == 'r':
-            self._pos[1] += 1
+            self.pos[1] += 1
         else:
             return False
         return True
@@ -52,15 +56,12 @@ class Piece:
         for r in range(len(self._shape)):
             for c in range(len(self._shape[0])):
                 if self._shape[r][c] != 0:
-                    occ.append((r + self._pos[0], c + self._pos[1]))
+                    occ.append((r + self.pos[0], c + self.pos[1]))
         return occ
 
     def act(self, action):
-        if action == 'cw':
-            self._rotate(1)
-            return True
-        elif action == 'ccw':
-            self._rotate(-1)
+        if action in ('cw', 'ccw'):
+            self._rotate(utils.ROTATION_TO_VAL[action])
             return True
         elif action == 'u' or action == 'd' or action == 'l' or action == 'r':
             self._move(action)
@@ -68,13 +69,17 @@ class Piece:
         return False
 
 class Board:
-
     # sets the board state and seeds the random generator
     def __init__(self, board=None, rseed=None):
         # initializes the physical board with 0's, representing empty in all spaces
         # if a prefilled board state is desired, it can be given
+
+        self.height = 26
+        self.playable_height = 20
+        self.width = 10
+
         if board is None:
-            self._board = np.array([[0 for i in range(10)] for i in range(20)])
+            self._board = np.array([[0 for i in range(self.width)] for i in range(self.height)])
         else:
             self._board = np.array(board)
 
@@ -83,20 +88,24 @@ class Board:
 
         random.seed(rseed)
 
+        self.lines_cleared = 0
         self.score = 0
         self.dead = False
         self.held_piece = None
         self._hold_used = False
 
         self.cur_piece = None
+        self.next_pieces = deque()
         self._pick_new_next()
         self._spawn_piece()
+        self.ghost_piece_occupied = None
+        self._generate_ghost_piece()
 
     # checks if the current location of the piece is valid
     #   i.e. doesn't intersect with already placed blocks
     def _piece_valid(self):
         for pos in self.cur_piece.occupied():
-            if not (0 <= pos[0] <= 19 and 0 <= pos[1] <= 9):
+            if pos[0] >= self.height or pos[1] not in range(self.width):
                 return False
             if self._board[pos] != 0:
                 return False
@@ -105,41 +114,24 @@ class Board:
     # picks a random new piece
     #   modify this if want to switch to bag/other randomizer
     def _pick_new_next(self):
-        self.next_piece = random.choice(list(utils.shapes))
+        self.next_pieces.extend(np.random.permutation(utils.SHAPES))
 
-    # spawns a new piece in (called after _lock_piece or at beginning)
+    # spawns a new piece in
     # also checks validity of spawned piece to see if game is lost
     def _spawn_piece(self, newpiece=None):
         self._hold_used = False
         if newpiece is None:
-            self.cur_piece = Piece(self.next_piece)
-            self._pick_new_next()
+            self.cur_piece = Piece(self.next_pieces.popleft())
+            if len(self.next_pieces) < 7:
+                self._pick_new_next()
         else:
             self.cur_piece = Piece(newpiece)
+        self._generate_ghost_piece()
 
         if not self._piece_valid():
             self.dead = True
             return False
         return True
-
-    # clears lines as needed and award points
-    def _clear_lines(self):
-        lcleared = 0
-        for r_ind in range(len(self._board)):
-            if all(val != 0 for val in self._board[r_ind]):
-                for l_ind in reversed(range(1, r_ind + 1)):
-                    self._board[l_ind] = self._board[l_ind - 1]
-                self._board[0] = 0
-                self.score += 1000 * (lcleared + 1)
-                lcleared += 1
-
-        return lcleared
-
-    # locks _cur_piece in place and spawns a new one
-    def _lock_piece(self):
-        for pos in self.cur_piece.occupied():
-            self._board[pos] = utils.shape_values[self.cur_piece.piece_str]
-        self._clear_lines()
 
     def _hold_piece(self):
         newpiece = self.held_piece
@@ -147,19 +139,48 @@ class Board:
         self._spawn_piece(newpiece=newpiece)
         self._hold_used = True
 
-    # public interface; this is how the player will interact
-
-    def should_lock(self):
+    def _generate_ghost_piece(self):
         og_piece = copy.deepcopy(self.cur_piece)
-        self.cur_piece.act('d')
+        while self._piece_valid():
+            self.cur_piece.act('d')
+        self.cur_piece.act('u')
 
-        res = not self._piece_valid()
+        self.ghost_piece_occupied = self.cur_piece.occupied()
         self.cur_piece = og_piece
 
-        return res
+    # clears lines as needed and award points
+    def _clear_lines(self):
+        lcleared = 0
+        for r_ind in range(self.height):
+            if all(val != 0 for val in self._board[r_ind]):
+                for l_ind in reversed(range(1, r_ind + 1)):
+                    self._board[l_ind] = self._board[l_ind - 1]
+                self._board[0] = 0
+                self.score += 1000 * (lcleared + 1)
+                lcleared += 1
+
+        self.lines_cleared += lcleared
+        return lcleared
+
+    # public interface; this is how the player will interact
+
+    # locks _cur_piece in place and spawns a new one
+    def lock_piece(self):
+        safe = False
+        for pos in self.cur_piece.occupied():
+            self._board[pos] = utils.shape_values[self.cur_piece.piece_str]
+            if pos[0] >= self.height - self.playable_height:
+                safe = True
+        if not safe:
+            self.dead = True
+        self._clear_lines()
+        self._spawn_piece()
 
     # valid actions: u, d, l, or r for movement
     # cw or ccw for rotation
+    # returns   -1 for invalid action
+    #           0 for failed action (e.g. piece became invalid)
+    #           1 for successful action
     def act(self, action):
         if action not in ['d', 'l', 'r', 'hd', 'cw', 'ccw', 'hold']:
             return -1
@@ -170,24 +191,34 @@ class Board:
             self._hold_piece()
             return 1
 
-        og_piece = None
         if action == 'hd':
             while self._piece_valid():
                 self.cur_piece.act('d')
             self.cur_piece.act('u')
-            og_piece = copy.deepcopy(self.cur_piece)
-            self.cur_piece.act('d')
-        else:
-            og_piece = copy.deepcopy(self.cur_piece)
-            self.cur_piece.act(action)
+            self.lock_piece()
+            return 1
+
+        og_piece = copy.deepcopy(self.cur_piece)
+        self.cur_piece.act(action)
 
         if not self._piece_valid():
+            if action in ('cw', 'ccw'):
+                rotations = utils.SRS_TABLE.get_rotation(og_piece.piece_str, og_piece.rotation, utils.ROTATION_TO_VAL[action])
+                for srs in rotations:
+                    old_pos = self.cur_piece.pos
+                    self.cur_piece.pos = list(utils.vector_add(self.cur_piece.pos, srs))
+                    if self._piece_valid():
+                        self._generate_ghost_piece()
+                        return 1
+                    else:
+                        self.cur_piece.pos = old_pos
+
             self.cur_piece = og_piece
-            if action == 'd' or action == 'hd':
-                self._lock_piece()
-                self._spawn_piece()
-            else:
-                return 0
+            return 0
+
+        if action != 'd':
+            self._generate_ghost_piece()
+
         return 1
 
     def state(self):
